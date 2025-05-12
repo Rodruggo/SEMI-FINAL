8023e4b735617bedfcdf9fa99d0d1e718f172c77
Rodruggo <araoarao.rodrigojr@cec.edu.ph>
2025-05-12 22:40:31 +0800

    change port again2x

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def home():
 #  app.run(debug=True, host='0,0,0,0',port=port)
 
 if __name__ == 'main':
- app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
+ app.run(debug=True, port=int(os.environ.get('PORT', 0000)))
