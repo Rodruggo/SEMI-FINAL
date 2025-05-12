98e09108b88bbe3aa6ad90923fa111a3469446e9
Rodruggo <araoarao.rodrigojr@cec.edu.ph>
2025-05-12 22:34:23 +0800

    change port again

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ def home():
 
 
 
-if __name__ == '__main__':
- port = int(os.environ.get("PORT", 0000))
- app.run(debug=True, host='0,0,0,0',port=port)
+# if __name__ == '__main__':
+#  port = int(os.environ.get("PORT", 0000))
+#  app.run(debug=True, host='0,0,0,0',port=port)
 
-# if __name__ == 'main':
-#  app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
+if __name__ == 'main':
+ app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
