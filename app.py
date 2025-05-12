f76d7c45e6d83789a6422d8520cc83d20512eac2
Rodruggo <araoarao.rodrigojr@cec.edu.ph>
2025-05-12 22:22:58 +0800

    change the return

--- app.py
+++ app.py
@@ -5,14 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return (
-        "Name: Rodrigo Araoarao\n"
-        "Section: BSIT-III-\n"
-        "Subject: System Integration\n"
-        "\nSemi -Final Exam"
+    return       "Name: Rodrigo Araoarao\n, Section: BSIT-III-\n, Subject: System Integration\n,\nSemi -Final Exam"
 
-    )
+
+
 
 if __name__ == '__main__':
  port = int(os.environ.get("PORT", 5000))
  app.run(debug=True, host='0,0,0,0',port=port)
+
+# if __name__ == 'main':
+#  app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
